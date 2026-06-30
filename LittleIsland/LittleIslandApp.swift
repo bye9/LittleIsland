0861c339b84546d441fd185d71c13985f0e4b3cb
JeongHwan Seok <bye_9@naver.com>
2026-06-30 19:44:38 +0900

    Initial Commit

---
+++ LittleIsland/LittleIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleIslandApp.swift
+//  LittleIsland
+//
+//  Created by JeongHwan Seok on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
